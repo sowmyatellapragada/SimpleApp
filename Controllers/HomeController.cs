90192fee8d92bf1f435369cdd49be9a791fadc97
sowmyatellapragada <sowmyatellapragada01@gmail.com>
2021-02-18 16:54:19 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimpleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using static SimpleApp.Models.Product;
+
+namespace SimpleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ViewResult Index()
+        //{
+        //    return View(Product.GetProducts());
+        //}
+        public IDataSource dataSource = new ProductDataSource();
+        public ViewResult Index()
+        {
+            return View(dataSource.Products);
+        }
+    }
+}
